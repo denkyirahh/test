1b4aa2eaef8afa1a1d1dc494b06ef9c80e985b62
michael.asomani <michael.asomani@GLDL00014273.goldlnk.rootlnka.net>
2025-05-27 10:53:47 -0400

    test 2

---
+++ tests/src/testspackage/plaqueboymax.java
@@ -0,0 +1,10 @@
+package testspackage;
+
+public class plaqueboymax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Revision 1");
+	}
+
+}
